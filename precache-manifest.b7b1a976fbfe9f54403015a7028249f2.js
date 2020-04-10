baf5e510ad06254363c061f0730a32c62db68d4a
AleksandrKaradzikov <karadzikovaleksandr@gmail.com>
2020-04-10 17:03:13 +0300

    Updates

---
+++ precache-manifest.b7b1a976fbfe9f54403015a7028249f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f75cf572edb85ad61a940948131638",
+    "url": "/kameleoon-testing/index.html"
+  },
+  {
+    "revision": "584dd54358b9ae737d2d",
+    "url": "/kameleoon-testing/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c0e2ae6d41d22d772460",
+    "url": "/kameleoon-testing/static/css/main.3e5a3cee.chunk.css"
+  },
+  {
+    "revision": "584dd54358b9ae737d2d",
+    "url": "/kameleoon-testing/static/js/2.6d3825e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kameleoon-testing/static/js/2.6d3825e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e2ae6d41d22d772460",
+    "url": "/kameleoon-testing/static/js/main.dde30c95.chunk.js"
+  },
+  {
+    "revision": "4ba86a87349f17bf541d",
+    "url": "/kameleoon-testing/static/js/runtime-main.6b19f57a.js"
+  }
+]);
